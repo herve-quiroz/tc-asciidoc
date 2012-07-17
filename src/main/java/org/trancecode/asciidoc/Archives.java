eb62ce892a7e76225bae75b26eeab360c767017a
Herve Quiroz <herve.quiroz@trancecode.org>
2012-07-17 21:52:12 +0200

    Initial import

---
+++ src/main/java/org/trancecode/asciidoc/Archives.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2012 TranceCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.trancecode.asciidoc;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closeables;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * Utility methods realted to archives.
+ * 
+ * @author Herve Quiroz
+ */
+public final class Archives
+{
+    private Archives()
+    {
+        // No instantiation
+    }
+
+    public static Iterable<File> unzip(final File archive, final File targetDirectory)
+    {
+        // TODO preconditions
+        try
+        {
+            final ImmutableList.Builder<File> extractedFiles = ImmutableList.builder();
+            final ZipFile zipFile = new ZipFile(archive);
+            for (final Enumeration<? extends ZipEntry> e = zipFile.entries(); e.hasMoreElements();)
+            {
+                final ZipEntry entry = e.nextElement();
+                extractedFiles.add(unzipEntry(zipFile, entry, targetDirectory));
+            }
+
+            return extractedFiles.build();
+        }
+        catch (final IOException e)
+        {
+            throw new IllegalStateException(String.format("cannot unzip %s to %s", archive, targetDirectory), e);
+        }
+    }
+
+    private static File unzipEntry(final ZipFile archive, final ZipEntry entry, final File targetDirectory)
+            throws IOException
+    {
+        if (entry.isDirectory())
+        {
+            final File directory = new File(targetDirectory, entry.getName());
+            mkdirs(directory);
+            return directory;
+        }
+
+        final File outputFile = new File(targetDirectory, entry.getName());
+        if (!outputFile.getParentFile().exists())
+        {
+            mkdirs(outputFile.getParentFile());
+        }
+
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
+        try
+        {
+            in = new BufferedInputStream(archive.getInputStream(entry));
+            out = new BufferedOutputStream(new FileOutputStream(outputFile));
+            ByteStreams.copy(in, out);
+        }
+        finally
+        {
+            Closeables.closeQuietly(out);
+            Closeables.closeQuietly(in);
+        }
+
+        return outputFile;
+    }
+
+    private static void mkdirs(final File directory)
+    {
+        if (!directory.mkdirs())
+        {
+            throw new RuntimeException("cannot create directory: " + directory);
+        }
+    }
+}
---
+++ src/main/java/org/trancecode/asciidoc/AsciiDoc.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2012 TranceCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.trancecode.asciidoc;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.python.core.PyString;
+import org.python.core.PySystemState;
+import org.python.google.common.collect.Lists;
+import org.python.util.PythonInterpreter;
+import org.trancecode.logging.Logger;
+
+/**
+ * @author Herve Quiroz
+ */
+public final class AsciiDoc
+{
+    private AsciiDoc()
+    {
+        // No instantiation
+    }
+
+    private static Logger LOG = Logger.getLogger(AsciiDoc.class);
+
+    private static final String ASCIIDOC_VERSION = "8.6.7";
+    private static final String ASCIIDOC_EXECUTABLE = "asciidoc.py";
+    private static final File ASCIIDOC_DEPLOYED_DIRECTORY;
+
+    static
+    {
+        final URL executableUrl = AsciiDoc.class.getResource("/asciidoc-" + ASCIIDOC_VERSION + "/"
+                + ASCIIDOC_EXECUTABLE);
+        Preconditions.checkState(executableUrl != null);
+        final String pattern = "jar:(file:.*)!.*";
+        Preconditions.checkState(executableUrl.toString().matches(pattern));
+        final File jarFile = new File(URI.create(executableUrl.toString().replaceAll(pattern, "$1"))).getAbsoluteFile();
+        final File tempDirectory;
+        try
+        {
+            tempDirectory = File.createTempFile("asciidoc.", ".directory");
+        }
+        catch (final IOException e)
+        {
+            throw new IllegalStateException(e);
+        }
+        tempDirectory.delete();
+        tempDirectory.mkdir();
+
+        Archives.unzip(jarFile, tempDirectory);
+        ASCIIDOC_DEPLOYED_DIRECTORY = new File(tempDirectory, "asciidoc-" + ASCIIDOC_VERSION);
+        Preconditions.checkState(ASCIIDOC_DEPLOYED_DIRECTORY.isDirectory());
+        System.setProperty("asciidoc.home", ASCIIDOC_DEPLOYED_DIRECTORY.getAbsolutePath());
+        LOG.debug("asciidoc.home = {}", ASCIIDOC_DEPLOYED_DIRECTORY);
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                FileUtils.deleteQuietly(ASCIIDOC_DEPLOYED_DIRECTORY);
+            }
+        });
+    }
+
+    private static File getAsciiDocExecutable()
+    {
+        final File executable = new File(ASCIIDOC_DEPLOYED_DIRECTORY, ASCIIDOC_EXECUTABLE);
+        System.setProperty("asciidoc.py", executable.getAbsolutePath());
+        return executable;
+    }
+
+    public static void toXhtml(final InputStream input, final OutputStream output)
+    {
+        final List<File> tempFiles = Lists.newArrayList();
+        try
+        {
+            final File sourceFile = File.createTempFile("source.", ".asciidoc");
+            tempFiles.add(sourceFile);
+            FileUtils.copyInputStreamToFile(input, sourceFile);
+            final File targetFile = File.createTempFile("target.", ".html");
+            toXhtml(sourceFile, targetFile);
+            FileUtils.copyFile(targetFile, output);
+        }
+        catch (final IOException e)
+        {
+            throw new IllegalStateException(e);
+        }
+        finally
+        {
+            for (final File file : tempFiles)
+            {
+                file.delete();
+            }
+        }
+    }
+
+    public static void toXhtml(final File sourceFile, final File targetFile)
+    {
+        final long startTime = System.currentTimeMillis();
+        final File asciidocExecutable = getAsciiDocExecutable();
+        try
+        {
+            final PySystemState sys = new PySystemState();
+            sys.argv.clear();
+            sys.argv.append(new PyString(getAsciiDocExecutable().getAbsolutePath()));
+            sys.argv.append(new PyString("-bxhtml11"));
+            sys.argv.append(new PyString("-atoc2"));
+            sys.argv.append(new PyString("-apygments"));
+            sys.argv.append(new PyString("-o" + targetFile.getAbsolutePath()));
+            sys.argv.append(new PyString(sourceFile.getAbsolutePath()));
+
+            final PythonInterpreter python = new PythonInterpreter(null, sys);
+            python.set("__file__", asciidocExecutable.getAbsolutePath());
+            final String executableContent = Files.toString(asciidocExecutable, Charsets.UTF_8);
+            python.exec(executableContent);
+        }
+        catch (final IOException e)
+        {
+            throw new IllegalStateException(e);
+        }
+        finally
+        {
+            LOG.trace("{} executed in {} ms", asciidocExecutable, System.currentTimeMillis() - startTime);
+        }
+    }
+}
